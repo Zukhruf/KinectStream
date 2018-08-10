4fd726c4aeb6f3a1f18548c6cbc7524e10b01d5e
ZukhrufMifta <zukhruf.m.f@gmail.com>
2018-08-10 17:37:01 +0700

    Initial commit

---
+++ KinectStream/ExtendFunction.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows.Media.Imaging;
+using Microsoft.Kinect;
+using System.Windows;
+
+namespace KinectStream
+{
+    public static class ExtendFunction
+    {
+        public static Joint ScaleTo(this Joint joint, double width, double height)
+        {
+            return ScaleTo(joint, width, height);
+        }
+
+        public static void DrawPoint(this Canvas canvas, ColorSpacePoint colorSpacePoint)
+        {
+            //2D coordinate
+            Point point2D = new Point();
+
+            point2D.X = colorSpacePoint.X;
+            point2D.Y = colorSpacePoint.Y;
+
+            Ellipse ellipse = new Ellipse
+            {
+                Width = 20, Height = 20, Fill = new SolidColorBrush(Colors.Red)
+            };
+
+            Canvas.SetLeft(ellipse, point2D.X - ellipse.Width / 2);
+            Canvas.SetTop(ellipse, point2D.Y - ellipse.Height / 2);
+
+            canvas.Children.Add(ellipse);
+        }
+    }
+}
---
+++ KinectStream/MainWindow.xaml.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Kinect;
+
+namespace KinectStream
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        KinectSensor _sensor;
+        MultiSourceFrameReader _reader;
+        IList<Body> _bodies;
+        Mode _mode = Mode.Color;
+
+        bool _displayBody = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _sensor = KinectSensor.GetDefault();
+            if (_sensor != null)
+            {
+                _sensor.Open();
+            }
+
+            _reader = _sensor.OpenMultiSourceFrameReader(FrameSourceTypes.Color |
+                                                         FrameSourceTypes.Depth |
+                                                         FrameSourceTypes.Infrared |
+                                                         FrameSourceTypes.Body);
+            _reader.MultiSourceFrameArrived += _reader_MultiSourceFrameArrived;
+        }
+
+        private void _reader_MultiSourceFrameArrived(object sender, MultiSourceFrameArrivedEventArgs e)
+        {
+            var reference = e.FrameReference.AcquireFrame();
+
+            //Color
+            using (var frame = reference.ColorFrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    if(_mode == Mode.Color)
+                        camera.Source = ToBitmap(frame);
+                }
+            }
+
+            //Depth
+            using (var frame = reference.DepthFrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    if (_mode == Mode.Depth)
+                    {
+                        camera.Source = ToBitmap(frame);
+                    }
+                }
+            }
+
+            //Infrared
+            using (var frame = reference.InfraredFrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    if (_mode == Mode.Infrared)
+                    {
+                        camera.Source = ToBitmap(frame);
+                    }
+                }
+            }
+
+            //Body
+            using (var frame = reference.BodyFrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    _bodies = new Body[frame.BodyFrameSource.BodyCount];
+                    frame.GetAndRefreshBodyData(_bodies);
+
+                    foreach (var body in _bodies)
+                    {
+                        if (body != null)
+                        {
+                            if (body.IsTracked)
+                            {
+                                foreach (Joint joint in body.Joints.Values)
+                                {
+                                    // 3D Coordinates in meter
+                                    CameraSpacePoint skeletonPoint = joint.Position;
+
+                                    if (_mode == Mode.Color)
+                                    {
+                                        //Skeleton to color mapping
+                                        ColorSpacePoint colorPoint = _sensor.CoordinateMapper.MapCameraPointToColorSpace(skeletonPoint);
+
+                                        if (_displayBody)
+                                        {
+                                            canvas.DrawPoint(colorPoint);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        #region ColorFrame
+        private ImageSource ToBitmap(ColorFrame frame)
+        {
+            int width = frame.FrameDescription.Width;
+            int height = frame.FrameDescription.Height;
+
+            PixelFormat format = PixelFormats.Bgr32;
+
+            byte[] pixels = new byte[width * height * ((PixelFormats.Bgr32.BitsPerPixel + 7) / 8)];
+
+            if (frame.RawColorImageFormat == ColorImageFormat.Bgra)
+            {
+                frame.CopyRawFrameDataToArray(pixels);
+            }
+            else
+            {
+                frame.CopyConvertedFrameDataToArray(pixels, ColorImageFormat.Bgra);
+            }
+
+            int stride = width * format.BitsPerPixel / 8;
+
+            return BitmapSource.Create(width, height, 96, 96, format, null, pixels, stride);
+        }
+        #endregion
+
+        #region DepthFrame
+        private ImageSource ToBitmap(DepthFrame frame)
+        {
+            int width = frame.FrameDescription.Width;
+            int height = frame.FrameDescription.Height;
+
+            ushort minDepth = frame.DepthMinReliableDistance;
+            ushort maxDepth = frame.DepthMaxReliableDistance;
+
+            ushort[] depthData = new ushort[width * height];
+            byte[] pixelData = new byte[width * height * (PixelFormats.Bgr32.BitsPerPixel + 7) / 8];
+
+            PixelFormat format = PixelFormats.Bgr32;
+
+            frame.CopyFrameDataToArray(depthData);
+
+            int colorIndex = 0;
+            for (int depthIndex = 0; depthIndex < depthData.Length; ++depthIndex)
+            {
+                ushort depth = depthData[depthIndex];
+                byte intensity = (byte)(depth >= minDepth && depth <= maxDepth ? depth : 0);
+
+                pixelData[colorIndex++] = intensity;
+                pixelData[colorIndex++] = intensity;
+                pixelData[colorIndex++] = intensity;
+
+                ++colorIndex;
+            }
+
+            int stride = width * format.BitsPerPixel / 8;
+            return BitmapSource.Create(width, height, 96, 96, format, null, pixelData, stride);
+        }
+        #endregion
+
+        #region InfraredFrame
+        private ImageSource ToBitmap(InfraredFrame frame)
+        {
+            int width = frame.FrameDescription.Width;
+            int height = frame.FrameDescription.Height;
+
+            PixelFormat format = PixelFormats.Bgr32;
+
+            ushort[] infraredData = new ushort[width * height];
+            byte[] pixelData = new byte[width * height * (format.BitsPerPixel + 7) / 8];
+
+            frame.CopyFrameDataToArray(infraredData);
+
+            int colorIndex = 0;
+            for (int infraredIndex = 0; infraredIndex < infraredData.Length; ++infraredIndex)
+            {
+                ushort ir = infraredData[infraredIndex];
+                byte intensity = (byte)(ir >> 8);
+
+                pixelData[colorIndex++] = intensity;
+                pixelData[colorIndex++] = intensity;
+                pixelData[colorIndex++] = intensity;
+
+                ++colorIndex;
+            }
+
+            int stride = width * format.BitsPerPixel / 8;
+
+            return BitmapSource.Create(width, height, 96, 96, format, null, pixelData, stride);
+        }
+        #endregion
+
+        private void Color_Click(object sender, RoutedEventArgs e)
+        {
+            _mode = Mode.Color;
+        }
+
+        private void Depth_Click(object sender, RoutedEventArgs e)
+        {
+            _mode = Mode.Depth;
+        }
+
+        private void Infrared_Click(object sender, RoutedEventArgs e)
+        {
+            _mode = Mode.Infrared;
+        }
+
+        private void Body_Click(object sender, RoutedEventArgs e)
+        {
+            _displayBody = !_displayBody;
+        }
+
+    }
+
+    #region Mode
+    public enum Mode {
+        Color,
+        Depth,
+        Infrared
+    }
+    #endregion
+}
